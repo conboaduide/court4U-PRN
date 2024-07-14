aed494fc4ffefffbd6228ce0aa678feb58409ac7
loctodale <thang336655@gmail.com>
2024-07-14 15:55:21 +0700

    add member subs

---
+++ Court4U/Pages/Owner/MemberSubscription/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Court4U.Pages.Owner.MemberSubscription
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Court4U/Pages/Owner/MemberSubscription/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Court4U.Pages.Owner.MemberSubscription
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
