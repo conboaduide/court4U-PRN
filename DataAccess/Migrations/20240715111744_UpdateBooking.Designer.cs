f58859dfc1ca84a8c740bbc31fb34e864a3d68e2
korroo <anhvnvse172006@fpt.edu.vn>
2024-07-15 18:19:12 +0700

    fix

--- DataAccess/Migrations/20240715111744_UpdateBooking.Designer.cs
+++ DataAccess/Migrations/20240715111744_UpdateBooking.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(Court4UDbContext))]
-    partial class Court4UDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240715111744_UpdateBooking")]
+    partial class UpdateBooking
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20240715111744_UpdateBooking.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookingId",
+                table: "BookedSlot",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookedSlot_BookingId",
+                table: "BookedSlot",
+                column: "BookingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookedSlot_Booking_BookingId",
+                table: "BookedSlot",
+                column: "BookingId",
+                principalTable: "Booking",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookedSlot_Booking_BookingId",
+                table: "BookedSlot");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookedSlot_BookingId",
+                table: "BookedSlot");
+
+            migrationBuilder.DropColumn(
+                name: "BookingId",
+                table: "BookedSlot");
+        }
+    }
+}
